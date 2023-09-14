d41020cb38364423578529b62b32d66908e5275a
Erick-Macedo-Costa <erickpart28@gmail.com>
2023-09-14 08:24:18 -0300

    atulizado

---
+++ Atividade2JPA/src/main/java/com/example/Atividade2JPA/Atividade2JpaApplication.java
@@ -0,0 +1,13 @@
+package com.example.Atividade2JPA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Atividade2JpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Atividade2JpaApplication.class, args);
+	}
+
+}
---
+++ Atividade2JPA/src/test/java/com/example/Atividade2JPA/Atividade2JpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Atividade2JPA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Atividade2JpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
