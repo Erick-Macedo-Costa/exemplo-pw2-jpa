07e016be1ab5b174b942845838dcfc5a47448f5b
Erick-Macedo-Costa <erickpart28@gmail.com>
2023-09-14 08:35:04 -0300

    Criando o novo projeto, Herença de Pessoa

---
+++ MapeamentoHeranca/src/main/java/com/example/MapeamentoHeranca/MapeamentoHerancaApplication.java
@@ -0,0 +1,13 @@
+package com.example.MapeamentoHeranca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapeamentoHerancaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MapeamentoHerancaApplication.class, args);
+	}
+
+}
