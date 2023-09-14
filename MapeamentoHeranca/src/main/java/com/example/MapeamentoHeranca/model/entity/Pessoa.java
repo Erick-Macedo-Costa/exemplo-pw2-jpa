67508d740186154e98c8c0810ac6fd7c0c9e8f8e
Erick-Macedo-Costa <erickpart28@gmail.com>
2023-09-14 08:51:05 -0300

    Colocado atributos no Pessoa

--- MapeamentoHeranca/src/main/java/com/example/MapeamentoHeranca/model/entity/Pessoa.java
+++ MapeamentoHeranca/src/main/java/com/example/MapeamentoHeranca/model/entity/Pessoa.java
@@ -1,4 +1,39 @@
 package com.example.MapeamentoHeranca.model.entity;
 
-public class Pessoa {
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+import java.io.Serializable;
+@Entity
+public abstract class Pessoa implements Serializable {
+    @Id
+    @GeneratedValue // (strategy = GenerationType.AUTO)
+    private  Long id;
+    private String endereco;
+    private String telefone;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
 }
